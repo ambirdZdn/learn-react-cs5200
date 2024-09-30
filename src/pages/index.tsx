44d49a0eb5f457c094586685413826a6be8948fb
Amber Z <zengdongniamber@gmail.com>
2024-09-30 17:12:57 -0400

    add fixed qcomp files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,25 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
 
+//import Profile from './qcomps/profile_mistake'
+//import MyComp from "./qcomps/firstcomp"
+//import Bio from "./qcomps/bios"
+//import TodoList from "./qcomps/todos"
+//import Profile from './components/profile_props'
+import Profile from './qcomps/gallery_props'
+import PackingList from './qcomps/props_item'
 export default function Home() {
   return (
-    <Profile />
+    <div>
+      {/* <Profile /> */} {/* fixed profile_mistake.tsx */}
+      {/* <MyComp /> */} {/* fixed firstcomp.tsx */}
+      {/*<Bio /> */} {/* fixed bios.tsx */}
+      {/*<TodoList /> */} {/* fixed todos.tsx */}
+      {/*<Profile /> */} {/* display profile_props.tsx */}  
+      <Profile /> 
+      {/*<PackingList /> */} {/* display props_item.tsx */}
+    </div>
+   
   )
 }
