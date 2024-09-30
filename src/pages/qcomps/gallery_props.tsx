0b34e0802ccfe250755dbb33fb5185ce42409d14
Amber Z <zengdongniamber@gmail.com>
2024-09-30 17:09:39 -0400

    add fixed qcomp files

--- src/pages/qcomps/gallery_props.tsx
+++ src/pages/qcomps/gallery_props.tsx
@@ -1,55 +1,66 @@
-export default function Gallery() {
+import {GalleryProps} from "@/types/gallery";
+import { ListItemProps } from "@/types/listItem";
+
+function ListItem(props: ListItemProps) {
+  return (
+    <li>
+      <b>{props.label}: </b>
+      {props.content}
+    </li>
+  );
+}
+
+function Gallery(props: GalleryProps) {
+  const {scientist, size=100} = props;
+  return (
+    <section className="profile">
+      <h2>{scientist.name}</h2>
+      <img
+        className="avatar"
+        src={'https://i.imgur.com/' + scientist.imageId + 's.jpg'}
+        alt={scientist.name}
+        width={size}
+        height={size}
+      />
+      <ul>
+        <ListItem label="Profession" content={scientist.profession} />
+        <ListItem label="Awards" content={scientist.awards} />
+        <ListItem label="Discovery" content={scientist.discovery} />
+      </ul>
+    </section>
+  );
+}
+
+export default function Profile() {
   return (
     <div>
-      <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <Gallery
+        scientist={{
+          name: 'Maria Skłodowska-Curie',
+          imageId: 'szV5sdG',
+          profession: 'physicist and chemist',
+          awards: '4 (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)',
+          discovery: 'polonium (element)',
+        }}
+      />
+      <Gallery
+        scientist={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2',
+          profession: 'geochemist',
+          awards: '2 (Miyake Prize for geochemistry, Tanaka Prize)',
+          discovery: 'a method for measuring carbon dioxide in seawater',
+        }}
+      />
+      <Gallery
+        scientist={{
+          name: 'Katherine Johnson',
+          imageId: '1bX5QH6',
+          profession: 'Mathematician',
+          awards: 'Presidential Medal of Freedom',
+          discovery: 'Trajectory analysis for space travel',
+        }}
+      />
     </div>
   );
 }
