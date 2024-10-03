98853f34e6ffa8c8149c147d19c8965e4e053c82
Amber Z <zengdongniamber@gmail.com>
2024-10-03 14:41:22 -0400

    update fixed qcomp files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,15 @@
 //import Profile from './components/profile_props'
 //import Profile from './qcomps/gallery_props'
 //import PackingList from './qcomps/props_item'
+//import List from './components/list_plain'
 //import List from './qcomps/list_keys_id'
 //import RecipeList from './qcomps/recipes'
+//import State from './qcomps/state'
+//import PersonData from './components/stateObj'
+//import Form from './qcomps/stuckForm'
+//import FeedbackForm from './qcomps/thankYouCrash'
+//import Counter from './components/snapshot'
+import Form from "./components/snapshot2"
 export default function Home() {
   return (
     <div>
@@ -22,8 +29,15 @@ export default function Home() {
       {/*<Profile /> */} {/* display profile_props.tsx */}  
       {/*<Profile /> */} {/* fixed gallery_props.tsx */}
       {/*<PackingList /> */} {/* display props_item.tsx */}
+      {/*<List /> */} {/* display list_plain.tsx */}
       {/*<List /> */} {/* display list_keys_id.tsx */}
       {/*<RecipeList /> */} {/* display recipes.tsx */}
+      {/*<State /> display state.tsx */}
+      {/*<PersonData /> display stateObj.tsx */}
+      {/* <Form /> display stuckForm.tsx */}
+      {/*<FeedbackForm />  display thankYouCrash.tsx */}
+      {/*<Counter />  display snapshot.tsx */}
+      <Form />  {/* display snapshot2.tsx */}
     </div>
    
   )
--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -12,8 +12,39 @@ export const recipes = [{
   ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
+function ingredientItemList({ingredients}: {ingredients: Array<string>}) {
+  return ingredients.map(ingredient => (
+        <li key={ingredient}>
+          {ingredient}
+        </li>
+      ));
+}
+
+function RecipeDiv({name, ingredients}: {name: string, ingredients: Array<string>}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredientItemList({ingredients})}
+      </ul>
+    </div>
+  );
+}
+  
+
 export default function RecipeList() {
   return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe => (
+        <RecipeDiv key={recipe.id} name={recipe.name} ingredients={[...recipe.ingredients]} />
+      )}
+    </div>
+  );
+}
+
+  
+    {/*
     <div>
       <h1>Recipes</h1>
       {recipes.map(recipe => (
@@ -26,6 +57,4 @@ export default function RecipeList() {
           </ul>
         </div>
       ))}
-    </div>
-  );
-}
+    </div>*/}
